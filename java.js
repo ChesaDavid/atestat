f36685a70fc906520723da24f3236e9e75c3c0e2
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-11-29 16:16:25 +0200

    Start of the app

---
+++ java.js
@@ -0,0 +1,18 @@
+let omeniDeZapada = [
+    {
+        nas:"morcovi",
+        ochi:"carbni",
+        status:"topit",
+    },
+    {
+        nas:"zmije",
+        ochi:"carbni",
+        status:"topit",
+    }
+];
+omeniDeZapada.push({
+    nas:"morcovi",
+    ochi:"carbni",
+    status:"topit",
+});
+console.log(omeniDeZapada);
