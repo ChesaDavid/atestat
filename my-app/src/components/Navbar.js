d559c0e965a10c0ebf9b1b0d424c3d91d6f2a18d
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-12-15 16:22:24 +0200

    android remake and plans are closer to being drawn

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "../pages/Home";
+import '../index.css';
 import { Link } from "react-router-dom";
 function Navbar(){
     return (
         <nav>
             <ul>
-                <li><Link to="/">Home</Link></li>
+                <li><Link to="/">Home</Link></li>    
             </ul>
         </nav>
     );
