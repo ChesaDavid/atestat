6f3f95be0da9a51c1515d543773598461e81595c
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-12-29 15:44:41 +0200

    restart

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "../pages/Home";
 import '../index.css';
+import Logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 function Navbar(){
     return (
         <nav>
+            <img src={Logo} alt="Logo" />
             <ul>
                 <li><Link to="/">Home</Link></li>    
             </ul>
